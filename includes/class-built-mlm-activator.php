3b095157941a58bb720a8fee7a6f4afa86dd6456
Aaron <aaron@forgue.net>
2015-04-11 16:13:00 +0000

    Initial commit of boilerplate files

---
+++ includes/class-built-mlm-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://pressbuilt.com
+ * @since      1.0.0
+ *
+ * @package    Built_Mlm
+ * @subpackage Built_Mlm/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Built_Mlm
+ * @subpackage Built_Mlm/includes
+ * @author     Pressbuilt <inquiries@pressbuilt.com>
+ */
+class Built_Mlm_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-built-mlm-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://pressbuilt.com
+ * @since      1.0.0
+ *
+ * @package    Built_Mlm
+ * @subpackage Built_Mlm/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Built_Mlm
+ * @subpackage Built_Mlm/includes
+ * @author     Pressbuilt <inquiries@pressbuilt.com>
+ */
+class Built_Mlm_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
